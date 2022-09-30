ac6b1e3c73a495d5680fb4acc626a47ac5866ce0
aryan16x <ary16.work@gmail.com>
2022-10-01 02:16:15 +0530

    Update script.js

--- Find-CSI/script.js
+++ Find-CSI/script.js
@@ -1,7 +1,13 @@
+var streak=0;
+
 function correct() {
 	alert("Correct ans");
+	streak++;
+	document.getElementById("box").innerHTML = streak;
 }
 
 function wrong() {
 	alert("Wrong ans");
+	streak=0;
+	document.getElementById("box").innerHTML = streak;
 }
