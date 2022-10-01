c101ba1290ee87de58fcd2070d92836ed7ed8781
Unknown-red7703 <padmajaatreya02@gmail.com>
2022-10-01 23:21:25 +0530

    Conflict resolve
    .

--- Find-CSI/script.js
+++ Find-CSI/script.js
@@ -7,6 +7,7 @@ function correct() {
 	document.getElementById("cs").innerHTML = streak;
 	document.getElementById("hs").innerHTML = highestStreak;
 	nextImage();
+	randomcsi();
 }
 
 function wrong() {
@@ -32,9 +33,33 @@ function nextImage(){
 	img=shinchanImages[Math.floor(Math.random()*shinchanImages.length)];
 	//Setting that random image
 	document.getElementById('shinchan').src=img;
+	var winWidth1 = window.innerWidth;
+	var winHeight1 = window.innerHeight;
+		
+		randomTop1 = getRandomNumber(0, winHeight1);
+		randomLeft1 = getRandomNumber(0, winWidth1);
+		
+		document.getElementById('shinchan').style.top= randomTop1 +"px";
+		document.getElementById('shinchan').style.left = randomLeft1 +"px";
 }
 
+function randomcsi(){
+	document.getElementById('csi').src='csi logo.jpg';
+	var winWidth2 = window.innerWidth;
+	var winHeight2 = window.innerHeight;
+		
+		randomTop2 = getRandomNumber(0, winHeight2);
+		randomLeft2 = getRandomNumber(0, winWidth2);
+		
+		document.getElementById('csi').style.top= randomTop2 +"px";
+		document.getElementById('csi').style.left = randomLeft2 +"px";
+}
 
+function getRandomNumber(min, max) {
+    
+	return Math.random() * (max - min) + min;
+	  
+}
 
 var score = 2;
 function strk() {
@@ -49,8 +74,6 @@ function strk() {
 		score = 2
 	}
 }
- 
-
 function add(submit) {
 	
 	var element = document.createElement("input");
