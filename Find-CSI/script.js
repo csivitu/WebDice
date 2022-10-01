e6f645b6353dee31aa0d544d956b032436335d24
SarveshN23 <93769009+SarveshN23@users.noreply.github.com>
2022-10-01 11:51:08 +0530

    darkmode(FindCSI) update

--- Find-CSI/script.js
+++ Find-CSI/script.js
@@ -8,5 +8,7 @@ function wrong() {
 
 function darkMode() {
 	var element = document.body;
+	var element2 = document.querySelector("h1");
 	element.classList.toggle("dark-mode");
+	element2.classList.toggle("dark-mode");
 }
