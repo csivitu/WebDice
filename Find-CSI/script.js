527533d95e895b908019043d88f3db23706617ec
Unknown-red7703 <padmajaatreya02@gmail.com>
2022-10-01 19:05:32 +0530

    Increasing choices
    This make a function that create new element on correct answer (upto 10 streak) and reset two 2 image on wrong ans.

--- Find-CSI/script.js
+++ Find-CSI/script.js
@@ -5,3 +5,34 @@ function correct() {
 function wrong() {
 	alert("Wrong ans");
 }
+var score = 2;
+function streak() {
+	for (var i = 1; i <= score; i++) {
+		add();
+	}
+
+	if (score < 1500) {
+		score = score * 2;
+	}
+	else {
+		score = 2
+	}
+}
+ 
+
+function add(submit) {
+	
+	var element = document.createElement("input");
+	 element.className="newimg"
+	element.type = submit; 
+	element.name = submit; 
+	element.onclick = function() {
+	  alert("Wrong");
+	  window.location.reload();
+	};
+  
+	var foo = document.getElementById("Spann");  
+	foo.appendChild(element);
+  }
+
+
