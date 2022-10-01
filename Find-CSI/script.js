d85c3211feca8f7f492583c1017decf6e48d83ca
Unknown-red7703 <padmajaatreya02@gmail.com>
2022-10-01 19:30:08 +0530

    Increase image
    this increase images (4,8,16...)on each correct answer and reset on wrong ans

--- Find-CSI/script.js
+++ Find-CSI/script.js
@@ -33,3 +33,36 @@ function nextImage(){
 	//Setting that random image
 	document.getElementById('shinchan').src=img;
 }
+
+
+
+var score = 2;
+function strk() {
+	for (var i = 1; i <= score; i++) {
+		add();
+	}
+
+	if (score < 1500) {
+		score = score * 2;
+	}
+	else {
+		score = 2
+	}
+}
+ 
+
+function add(submit) {
+	
+	var element = document.createElement("input");
+	 element.className="newimg"
+	element.type = submit; 
+	element.name = submit; 
+	element.onclick = function() {
+	  alert("Wrong");
+	  window.location.reload();
+	};
+	
+  
+	var foo = document.getElementById("Spann");  
+	foo.appendChild(element);
+  }
